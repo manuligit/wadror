70541614fe294dafdf158d948dc81f31c0b4cba4
manuligit <riina.vaisanen@gmail.com>
2014-01-26 16:27:33 +0200

    Excercises 7-14

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,8 +1,13 @@
 class Beer < ActiveRecord::Base
-  has_many :ratings
+  has_many :ratings, dependent: :destroy
   belongs_to :brewery
 
   def average_rating
     ratings.average('score')
   end
+
+  def to_s
+    "#{name} #{brewery.name}"
+  end
+
 end
